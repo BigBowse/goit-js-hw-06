d54a3e5f81ff3fe63cbefdc8a767ba5dabdc8bdc
Serhii Bohosta <bigbowse@Serhiis-MacBook-Pro.local>
2022-07-12 00:55:42 +0300

    done

---
+++ src/js/task-01.js
@@ -0,0 +1,19 @@
+const ulEl = document.querySelector('ul#categories');
+// const categories = document.querySelectorAll('.item');
+const categories = ulEl.children;
+console.log(`Number of categories: ${categories.length}`);
+
+const categoriesNames = document.querySelectorAll('h2');
+const firstCategoryName = categoriesNames[0];
+console.log(`Category: ${firstCategoryName.textContent}`);
+
+const categoriesLi = categories[0].querySelectorAll('li');
+console.log(`Elements: ${categoriesLi.length}`);
+
+console.log(`Category: ${categoriesNames[1].textContent}`);
+const categoriesLiOne = categories[1].querySelectorAll('li');
+console.log(`Elements: ${categoriesLiOne.length}`);
+
+console.log(`Category: ${categoriesNames[2].textContent}`);
+const categoriesLiTwo = categories[2].querySelectorAll('li');
+console.log(`Elements: ${categoriesLiTwo.length}`);
