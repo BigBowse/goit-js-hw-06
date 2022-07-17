32cdd1dd2289c7e053c0e6f2e8504a65dd390e38
Serhii Bohosta <bigbowse@Serhiis-MacBook-Pro.local>
2022-07-17 17:25:13 +0300

    done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,36 @@
+const form = document.querySelector('form.login-form');
+const submitBtn = form.lastElementChild;
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        Email: email,
+        Password: password,
+    }
+
+    if ( email === "" || password === "") {
+        window.alert('Please, fill in all the necessary fields');
+    }
+
+    console.log(formData);
+    form.reset();
+    
+
+    // const formData = new FormData(event.currentTarget);
+
+    // formData.forEach((value, name) => {
+    //     console.log('Name', name);
+    //     console.log('Value', value);
+    // })
+}
+
+
+
 
