8bbc056e407081f830ab7cd6d3b3af06653e6e05
Serhii Bohosta <bigbowse@Serhiis-MacBook-Pro.local>
2022-07-17 18:03:37 +0300

    done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+// const widget = document.querySelector('.widget');
+const colorNameLabel = document.querySelector('.color');
+const chngColorBtn = document.querySelector('.change-color');
+
+chngColorBtn.addEventListener('click', onBtnClk);
+
+function onBtnClk(event) {
+  const bgColor = getRandomHexColor();
+  body.style.backgroundColor = bgColor;
+  colorNameLabel.textContent = bgColor;
+}
