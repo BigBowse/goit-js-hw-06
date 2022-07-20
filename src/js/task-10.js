918de462f21b3a05524b64d60803d3fa3167f4b8
Serhii Bohosta <bigbowse@Serhiis-MacBook-Pro.local>
2022-07-20 22:10:36 +0300

    needs to be reviewed by mentor

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,29 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// const input = document.querySelector('#controls input');
+const createBtnEl = document.querySelector('[data-create]');
+const destroyBtnEl = document.querySelector('[data-destroy]');
+const boxes = document.getElementById('boxes');
+const amount = +document.querySelector('#controls input').value;
+createBtnEl.addEventListener('click', createBoxes);
+destroyBtnEl.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const firstDivSize = 30;
+
+  amount.forEach((number) => {
+    const div = document.createElement('div');
+    const size = firstDivSize + number * 10;
+    div.style.width = size + 'px';
+    div.style.height = size + 'px';
+    const bgColor = getRandomHexColor();
+    div.style.backgroundColor = bgColor;
+    boxes.appendChild(div);
+  });
+} 
+
+function destroyBoxes() {
+  boxes.innerHTML = "";
+}
